dbce6126212e1d1d1cd1410ca3b846ef099deff0
br-on <bhenriquedagama@gmail.com>
2025-04-24 12:34:25 -0300

    correcao <

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ document.getElementById("btnBuscarDemanda").addEventListener("click", async () =
         const c = chamado[0];
         
         const html = `
-            <<table border="1" cellpadding="8">
+            <table border="1" cellpadding="8">
                 <tr><th>Nº Demanda</th><td>${c.n_demanda}</td></tr>
                 <tr><th>Solicitante</th><td>${c.solicitante}</td></tr>
                 <tr><th>Unidade</th><td>${c.us}</td></tr>
